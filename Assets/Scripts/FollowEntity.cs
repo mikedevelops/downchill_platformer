82acd2627ab57da9bad321d9028686529cb03c9e
Mike Smart <mike.smart@jadu.co.uk>
2018-07-17 09:28:47 +0100

    intiial commit

---
+++ Assets/Scripts/FollowEntity.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FollowEntity : MonoBehaviour
+{
+    public Transform Target;
+    public float Speed = 0.125f;
+    public Vector2 Offset;
+
+    private void Update()
+    {
+        Vector2 desiredPosition = new Vector2(Target.position.x, Target.position.y) + Offset;
+        Vector2 smoothedPosition = Vector2.Lerp(transform.position, desiredPosition, Speed);
+        transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Range(0.1f, 1)]
+    public float TopSpeed;
+    
+    [Range(0.01f, 0.1f)]
+    public float Acceleration;
+
+    private Rigidbody2D _rigidbody2D;
+    
+    private float _currentSpeed;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector2 v = _rigidbody2D.velocity;
+            
+            if (_currentSpeed < TopSpeed)
+            {
+                _currentSpeed = Mathf.Clamp(_currentSpeed + Acceleration, 0, TopSpeed);
+            }
+
+            v.x += _currentSpeed;
+            _rigidbody2D.velocity = v;
+        }
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector2 v = _rigidbody2D.velocity;
+            
+            if (_currentSpeed < TopSpeed)
+            {
+                _currentSpeed = Mathf.Clamp(_currentSpeed + Acceleration, 0, TopSpeed);
+            }
+
+            v.x -= _currentSpeed;
+            _rigidbody2D.velocity = v;
+        }
+
+    }
+}
